b2b16eb1ea115f360fcbc3873a6df6c8295be8df
Volodymyr Tieliezhkin <57314700+Volodymyr-T@users.noreply.github.com>
2022-02-17 17:55:05 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@
 
 module.exports = function towelSort (matrix) {
   if (!matrix) return [];
-  for (i = 0; i < matrix.length; i++) if (i % 2 != 0) matrix[i].reverse();
+  for (let i = 0; i < matrix.length; i++) if (i % 2 != 0) matrix[i].reverse();
   return matrix.flat();
 }
